385b792e10f11267cdccf88846d676c51dc3bcdf
Rapolas Senkus <rapolas.senkus@gmail.com>
2026-07-13 01:42:52 +0300

    Made some changes and added some logic to js file to make the web functional

--- script.js
+++ script.js
@@ -2,25 +2,15 @@ const addBookBtn = document.getElementById("AddBtn");
 const dialog = document.getElementById("my-dialog");
 const closeBtn = document.getElementById("closeBtn");
 const mainContent = document.getElementById("main");
-
 const submit = document.getElementById("submit-btn");
 
 addBookBtn.addEventListener("click", () => {
     dialog.showModal();
 });
 
-
 closeBtn.addEventListener("click", () => {
     dialog.close();
 });
-
-function createTheCard(book){
-    const div = document.createElement("div");
-    div.classList.add("card");
-
-    mainContent.appendChild(div);
-
-}
 
 class Book {
     constructor(name, author, pages, readOrNot) {
@@ -30,47 +20,99 @@ class Book {
         this.readOrNot = readOrNot;
         this.id = crypto.randomUUID();
     }
-
-    static fromExisting(existingBook) {
-        const book = new Book(existingBook.name, existingBook.author, existingBook.pages, existingBook.readOrNot);
-        book.id = existingBook.id;
-        return book;
-    }
 }
 
 class Library {
     #bookList;
 
     constructor(listOfBooks = []) {
-        this.#bookList = listOfBooks.map(book => Book.fromExisting(book));
+        this.#bookList = listOfBooks.map(book => new Book(book.name, book.author, book.pages, book.readOrNot));
+    }
+
+    get books() {
+        return this.#bookList;
     }
 
     addBook(book) {
-        return this.#bookList.push(Book.fromExisting(book));
+        return this.#bookList.push(book);
     }
 
-    removeBook(book) {
-        this.#bookList = this.#bookList.filter(b => b.id !== book.id);
+    removeBookAt(index) {
+        this.#bookList.splice(index, 1);
     }
-    
+
+    toggleReadAt(index) {
+        this.#bookList[index].readOrNot = !this.#bookList[index].readOrNot;
+    }
 }
 
 const myLibrary = new Library();
+
+function renderLibrary() {
+    mainContent.innerHTML = "";
+
+    myLibrary.books.forEach((book, index) => {
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.dataset.indexNumber = index;
+
+        const btnClass = book.readOrNot ? "read" : "unread";
+        const btnText = book.readOrNot ? "Read" : "Unread";
+
+        div.innerHTML = `
+            <div class="card-content">
+                <h4>Name of the book:</h4>
+                <p>${book.name}</p>
+                
+                <h4>Author of the book:</h4>
+                <p>${book.author}</p>
+                
+                <h4>Total pages in the book:</h4>
+                <p>${book.pages}</p>
+            </div>
+            <div class="buttons">
+                <button class="status-btn ${btnClass}">${btnText}</button>
+                <button class="status-btn remove-btn">Remove</button>
+            </div>
+        `;
+
+        mainContent.appendChild(div);
+    });
+}
+
+mainContent.addEventListener("click", (event) => {
+    const card = event.target.closest(".card");
+    if (!card) return;
+
+    const index = Number(card.dataset.indexNumber);
+
+    if (event.target.classList.contains("remove-btn")) {
+        myLibrary.removeBookAt(index);
+        renderLibrary();
+        return;
+    }
+
+    if (event.target.classList.contains("status-btn")) {
+        myLibrary.toggleReadAt(index);
+        renderLibrary();
+    }
+});
+
 submit.addEventListener("click", (event) => {
     event.preventDefault();
 
-    let name = document.getElementById("name").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+    const nameValue = document.getElementById("name").value;
+    const authorValue = document.getElementById("author").value;
+    const pagesValue = document.getElementById("pages").value;
+    const readValue = document.getElementById("read").checked;
 
-    const newBook = new Book(name, author, pages, read);
-    // Rekomenduojama: įsidedam knygą į bibliotekos sekimą
-    myLibrary.addBook(newBook); 
+    myLibrary.addBook(new Book(nameValue, authorValue, pagesValue, readValue));
+    renderLibrary();
 
-    // Sukuriam vizualią kortelę
-    createTheCard(newBook);
-    
-    // Uždarom dialogą po sėkmingo pridėjimo
+    document.getElementById("name").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
+
     dialog.close();
-})
+});
